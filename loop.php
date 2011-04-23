973583e06ac79adc751100ca41e60ed2209aeb63
Jonathan Rothwell <jonathan@notroswell.com>
2011-04-23 02:15:01 +0100

    added entity/utility hangers to main posts

--- loop.php
+++ loop.php
@@ -221,22 +221,30 @@
 	<?php endif; ?>
 
 			<div class="entry-utility">
-				&#x2731;&nbsp;
-				<?php if ( count( get_the_category() ) ) : ?>
-					<span class="cat-links">
-						<?php printf( __( '<span class="%1$s">Posted in</span> %2$s', 'twentyten' ), 'entry-utility-prep entry-utility-prep-cat-links', get_the_category_list( ', ' ) ); ?>
-					</span>
-					<span class="meta-sep">|</span>
-				<?php endif; ?>
-				<?php
-					$tags_list = get_the_tag_list( '', ', ' );
-					if ( $tags_list ):
-				?>
-					<span class="tag-links">
-						<?php printf( __( '<span class="%1$s">Tagged</span> %2$s', 'twentyten' ), 'entry-utility-prep entry-utility-prep-tag-links', $tags_list ); ?>
-					</span>
-				<?php endif; ?>
-				<?php edit_post_link( __( 'Edit', 'twentyten' ), '<span class="meta-sep">|</span> <span class="edit-link">', '</span>' ); ?>
+				<div class="hanger-total">
+					<div class="entity-hanger">
+						&#x2731;&nbsp;
+					</div>
+					<div class="utility-hanger">
+						<?php if ( count( get_the_category() ) ) : ?>
+							<span class="cat-links">
+								<?php printf( __( '<span class="%1$s">Posted in</span> %2$s', 'twentyten' ), 'entry-utility-prep entry-utility-prep-cat-links', get_the_category_list( ', ' ) ); ?>
+							</span>
+							<span class="meta-sep">|</span>
+						<?php endif; ?>
+						<?php
+						$tags_list = get_the_tag_list( '', ', ' );
+						if ( $tags_list ):
+						?>
+							<span class="tag-links">
+								<?php printf( __( '<span class="%1$s">Tagged</span> %2$s', 'twentyten' ), 'entry-utility-prep entry-utility-prep-tag-links', $tags_list ); ?>
+							</span>
+						<?php endif; ?>
+						<?php edit_post_link( __( 'Edit', 'twentyten' ), '<span class="meta-sep">|</span> <span class="edit-link">', '</span>' ); ?>
+					</div>
+				</div>
+				
+				
 			</div><!-- .entry-utility -->
 		</div><!-- #post-## -->
 
