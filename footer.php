461e2e14f4cf3ff2051dee8def6c1d862fa63370
Jonathan Rothwell <jonathan@notroswell.com>
2011-05-30 20:05:58 +0100

    added image to licence

--- footer.php
+++ footer.php
@@ -23,7 +23,7 @@
 
 			<div id="site-generator">
 				<p><a href="http://wordpress.org/" rel="generator" title="Proudly powered by WordPress"><img src="http://notroswell.com/wp-content/themes/twentypie/images/w.png" alt="WordPress"/></a> <img src="http://notroswell.com/wp-content/themes/twentypie/images/plus.png" alt="+" /> <a href="http://nearlyfreespeech.net" title="Hosted at NearlyFreeSpeech.Net"><img src="http://notroswell.com/wp-content/themes/twentypie/images/n.png" title="NearlyFreeSpeech.NET" /></a> <img src="http://notroswell.com/wp-content/themes/twentypie/images/eq.png" alt="equals" /> <img src="http://notroswell.com/wp-content/themes/twentypie/images/heart.png" alt="Unadulterated pure brilliance" /></p>
-				<p>&copy; 1992&mdash;2011 Jonathan Rothwell.<span class="sep"> | </span><a rel="license" href="http://creativecommons.org/licenses/by-nc/3.0/">CC-BY-NC</a></p>
+				<p>&copy; 1992&mdash;2011 Jonathan Rothwell.<span class="sep"> | </span><a rel="license" href="http://creativecommons.org/licenses/by-nc/3.0/"><img src="http://notroswell.com/wp-content/themes/twentypie/images/eq.png" alt="CC-BY-SA" title="Licenced under CC-BY-SA 3.0" /></a></p>
 			</div>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
