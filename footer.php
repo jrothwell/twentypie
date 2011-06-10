8e308e0817ec53a66f6f7c29b5da71eaa7e5657f
Jonathan Rothwell <jonathan@notroswell.com>
2011-06-10 15:12:49 +0100

    added changes for cookies

--- footer.php
+++ footer.php
@@ -23,6 +23,7 @@
 
 			<div id="site-generator">
 				<p><a href="http://wordpress.org/" rel="generator" title="Proudly powered by WordPress"><img src="http://notroswell.com/wp-content/themes/twentypie/images/w.png" alt="WordPress"/></a> <img src="http://notroswell.com/wp-content/themes/twentypie/images/plus.png" alt="+" /> <a href="http://nearlyfreespeech.net" title="Hosted at NearlyFreeSpeech.Net"><img src="http://notroswell.com/wp-content/themes/twentypie/images/n.png" title="NearlyFreeSpeech.NET" /></a> <img src="http://notroswell.com/wp-content/themes/twentypie/images/eq.png" alt="equals" /> <img src="http://notroswell.com/wp-content/themes/twentypie/images/heart.png" alt="Unadulterated pure brilliance" /></p>
+				<div class="cookies"><strong>Cookie compliance:</strong> Please note that this Web site makes use of cookies to gather anonymous statistical information and store information about your session if you are logged in. If you do not wish to use these cookies, please disable cookies in your browser.</div>
 				<div><span style="vertical-align:middle;height:16px;">&copy; 1992&mdash;2011 Jonathan Rothwell. &nbsp; <a rel="license" href="http://creativecommons.org/licenses/by-nc/3.0/"></span><img src="http://notroswell.com/wp-content/themes/twentypie/images/cc-by-nc.png" alt="CC-BY-SA" title="Licenced under CC-BY-NC 3.0" style="vertical-align:middle" /></a></div>
 			</div>
 	</footer><!-- #colophon -->
