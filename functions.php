2fc55c1c6dabf5517878f27ebc4238d1b979b38a
Jonathan Rothwell <jonathan@notroswell.com>
2011-09-16 23:06:00 +0100

    changed header image width/height

---
+++ functions.php
@@ -0,0 +1,7 @@
+<?
+/* functions.php
+ * Part of Twenty Pie, for WordPress
+ */
+define( 'HEADER_IMAGE_WIDTH', apply_filters( 'twentyeleven_header_image_width', 1000 ) );
+define( 'HEADER_IMAGE_HEIGHT', apply_filters( 'twentyeleven_header_image_height', 100 ) );
+?>
