7165e0cf0fb75d4f014a9ce6263753f77fc8cc08
Jonathan Rothwell <jonathan@notroswell.com>
2011-09-16 23:07:46 +0100

    changed header image width/height to actual one

--- functions.php
+++ functions.php
@@ -3,5 +3,5 @@
  * Part of Twenty Pie, for WordPress
  */
 define( 'HEADER_IMAGE_WIDTH', apply_filters( 'twentyeleven_header_image_width', 1000 ) );
-define( 'HEADER_IMAGE_HEIGHT', apply_filters( 'twentyeleven_header_image_height', 100 ) );
+define( 'HEADER_IMAGE_HEIGHT', apply_filters( 'twentyeleven_header_image_height', 180 ) );
 ?>
